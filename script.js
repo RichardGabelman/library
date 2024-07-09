205fd9e1574c011f9fbcc37381c388f1ce908ef5
78068360+RichardGabelman@users.noreply.github.com <78068360+actuallychaz@users.noreply.github.com>
2024-07-08 17:05:00 -0700

    Implement displayLibrary function

--- script.js
+++ script.js
@@ -22,5 +22,39 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function displayLibrary() {
-  myLibrary.forEach(book => book.info());
+  const cardGrid = document.querySelector(".cards");
+  for (let i = 0; i < myLibrary.length; i++) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const title = document.createElement("div");
+    title.textContent = "Title: " + myLibrary[i].title;
+    card.appendChild(title);
+
+    const author = document.createElement("div");
+    author.textContent = "Author: " + myLibrary[i].author;
+    card.appendChild(author);
+
+    const pages = document.createElement("div");
+    pages.textContent = "Pages: " + myLibrary[i].pages;
+    card.appendChild(pages);
+
+    const read = document.createElement("div");
+    let readStatus;
+    if (myLibrary[i].read) {
+      readStatus = "✓";
+    } else {
+      readStatus = "X";
+    }
+    read.textContent = "Read: " + readStatus;
+    card.appendChild(read);
+
+    cardGrid.appendChild(card);
+  }
 }
+
+addBookToLibrary("Bob's Cook", "Bob", 522, true);
+addBookToLibrary("Bob's Cook", "Bob", 522, true);
+addBookToLibrary("Bob's Cook", "Bob", 522, true);
+
+displayLibrary();
